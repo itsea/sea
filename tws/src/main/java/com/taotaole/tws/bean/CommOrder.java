b06f105ecee11a9e41200c28d2c4b231e4a9975c
HS-smile <892775892@qq.com>
2019-11-17 18:38:39 +0800

    上传订单实体类

---
+++ tws/src/main/java/com/taotaole/tws/bean/CommOrder.java
@@ -0,0 +1,31 @@
+package com.taotaole.tws.bean;
+
+public class CommOrder {
+    private Integer cid;
+    private Integer idcode;
+    private Integer cidcode;
+
+    public Integer getCid() {
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+
+    public Integer getIdcode() {
+        return idcode;
+    }
+
+    public void setIdcode(Integer idcode) {
+        this.idcode = idcode;
+    }
+
+    public Integer getCidcode() {
+        return cidcode;
+    }
+
+    public void setCidcode(Integer cidcode) {
+        this.cidcode = cidcode;
+    }
+}
